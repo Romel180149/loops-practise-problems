8c02a3be672213deb59f1a9e7cfcbbf087b39f76
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-31 12:10:06 +0600

    continue code

---
+++ continue.js
@@ -0,0 +1,12 @@
+// for(var i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+var numbers=[10,20,30,40,50,60,70,80,90,100];
+for(var i = 0; i < numbers.length ; i++){
+    var number = numbers[i];
+    if(number>50){
+        continue;
+    }
+    console.log(number);
+}
