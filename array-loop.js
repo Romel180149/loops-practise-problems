6290b94f2931bd5e87d7d7897aabbd5e176c1efc
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-31 11:52:10 +0600

    loops problem js

---
+++ array-loop.js
@@ -0,0 +1,9 @@
+// for(var i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+// var numbers=[10,20,30,40,50,60,70,80,90,100];
+// for(var i = 0; i < numbers.length ; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// // }
---
+++ revision.js
@@ -0,0 +1,12 @@
+// string type variable 
+var bottle='water';
+var bottleColor='yellow';
+// number type variab
+var sunglassPirce=120;
+var penColor='blue';
+// boollean type
+var isExpensive ='true';
+var tableItems =['bottle', 'pen',  'mouse', 'sunglass', 'paper'];
+var item3 = tableItems[3];
+tableItems[4]='notebook';
+var penIndex=tableItems.indexOf('pen');
